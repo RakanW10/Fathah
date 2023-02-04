1f2b2d3657277965d81a52ede5d89e4b76167c10
Rakan Alotaibi <rakanalotaibi@Rakans-MacBook-Pro.local>
2023-02-05 02:18:54 +0300

    Added firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:fathah/firebase_options.dart';
 import 'package:fathah/router/router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
